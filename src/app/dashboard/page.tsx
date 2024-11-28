2d818b7698742eda99c39b6c295e1d7db359c543
Aldo D'Arrigo <aldodarrigos@gmail.com>
2024-11-28 12:32:11 +0100

    fixing delay

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { AppLineChart, AppBarChart, AppDoughnutChart, AppRadarChart } from "@/components";
 
 async function getData() {
-  await new Promise((resolve) => setTimeout(resolve, 2000)); await new Promise((resolve) => setTimeout(resolve, 4000));  // remove this line :)
+  await new Promise((resolve) => setTimeout(resolve, 2000)); 
   return {
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
     data: [1, 2, 3, 2, 5]
@@ -9,7 +9,7 @@ async function getData() {
 }
 
 async function getRadarData() {
-    await new Promise((resolve) => setTimeout(resolve, 2000)); await new Promise((resolve) => setTimeout(resolve, 4000));  // remove this line :)
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
   return {
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
